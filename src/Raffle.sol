05bec9086603c3e67752531db01bc4aa9cfedd17
wemmy6673 <okunolawemzy@gmail.com>
2025-08-02 20:54:27 +0100

    Array reset

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,6 +29,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
     
 
     event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint256 subscriptionId, uint32 callbackGasLimit) 
     VRFConsumerBaseV2Plus(vrfCoordinator) {
@@ -87,11 +88,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         s_raffleState = RaffleState.OPEN;
         (bool success, ) = recentWinner.call{value:address(this).balance}("");
         if (!success) {
             revert Raffle_TransferFailed();
         }
+        emit WinnerPicked(recentWinner);
     }
 
     function getEntranceFee() external view returns (uint256) {
