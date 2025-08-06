f2b92236b2e3381729326dc8b28839ea57cea04b
wemmy6673 <okunolawemzy@gmail.com>
2025-08-06 20:06:38 +0100

    deploying a mock chainlink vrf

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -8,5 +8,21 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployRaffle is Script {
     function run() public {}    
 
-    function deployContract()public returns (Raffle, HelperConfig){}
+    function deployContract()public returns (Raffle, HelperConfig){
+
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast(); 
+        return (raffle, helperConfig);
+}
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -54,6 +54,10 @@ contract HelperConfig is CodeConstants, Script {
         }
     }
 
+    function getConfig() public returns(NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) { 
         return NetworkConfig({
             entranceFee: 0.01 ether,
